7d755b781351c520ea8ef04f59fee208c73709d4
Francisco Escobedo <adriesc57@gmail.com>
2023-05-09 18:01:08 -0600

    se empieza en main proyect y se trabaja el password generator

---
+++ passwordgen.py
@@ -0,0 +1,25 @@
+#Generador random de contrasenhas
+#Adriesc57
+import random
+
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+ 'Y', 'Z'] 
+números = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+símbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+'] 
+
+print("Bienvenido al generador de contrasenhas")
+
+nrletras = int(input("Cuantas letras tendra la contrasenha?\n"))
+nrsimb = int(input("Cuantos simbolos tendra la contrasenha?\n"))
+nrnumeros = int(input("Cuantos numeros tendra la contrasenha?\n"))
+password =""
+
+for i in range(1, nrletras +1):
+    password += random.choice(letras)
+    
+for i in range(1, nrsimb + 1):
+    password += random.choice(símbolos)
+    
+for i in range(1, nrnumeros + 1):
+    password += random.choice(números)
+print(password)
